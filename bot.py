82020f63d47f4f09f8e7d8ba3d58c5f978de3474
zdarova69 <113101818+zdarova69@users.noreply.github.com>
2023-07-01 17:24:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,115 +1,125 @@
 import telebot
 from telebot import types
 from pixellib.instance import instance_segmentation
+from google_images_search import GoogleImagesSearch
+
+
 import os
-bot = telebot.TeleBot(token)
-#кнопки
-markup = types.InlineKeyboardMarkup(row_width=2)
-item0 = types.InlineKeyboardButton(text="все объекты", callback_data="все объекты")
-item1 = types.InlineKeyboardButton(text="людей", callback_data="людей")
-item2 = types.InlineKeyboardButton(text="машины", callback_data="машины")
-item3 = types.InlineKeyboardButton(text="животных", callback_data="животных")
-kb = markup.add(item0, item1, item2,item3)
+class MyBot:
+    def __init__(self):
+        self.token = "6095239590:AAGLodvJdqeE7EK-dfiFtZgsr8WtSwwd1-4"
+        self.bot = telebot.TeleBot(self.token)
+        self.name_photo = ''
+        self.markup1 = types.InlineKeyboardMarkup(row_width=2)
+        self.markup2 = types.InlineKeyboardMarkup(row_width=2)
+        self.item0 = types.InlineKeyboardButton(text="все объекты", callback_data="все объекты")
+        self.item1 = types.InlineKeyboardButton(text="людей", callback_data="людей")
+        self.item2 = types.InlineKeyboardButton(text="машины", callback_data="машины")
+        self.item3 = types.InlineKeyboardButton(text="животных", callback_data="животных")
+        self.item4 = types.InlineKeyboardButton(text="поиск по фото", callback_data="поиск")
+        self.kb = self.markup1.add(self.item0, self.item1, self.item2, self.item3)
+        self.kb2 = self.markup2.add(self.item4)
+    def run(self):
+        @self.bot.message_handler(commands=['start'])
+        def welcome(message):
+            self.bot.reply_to(message, "Привет! Я бот для поиска объектов. Отправь мне фото:")
 
-
-@bot.message_handler(commands=['start'])
-def welcome(message):
-    #markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    #item1 = types.KeyboardButton(text="найди чтото на картинке")
-    #item2 = types.KeyboardButton(text="Аоаоаоао")
-
-    #kb = markup.add(item1, item2) 
-    bot.send_message(message.chat.id, 'Здравствуйте. Я - бот,предназначенный находить объекты на фотографиях.отправь мне фото')# reply_markup=kb)
-    #kb = telebot.types.ReplyKeyboardRemove()
-
-#запрашиваем фото
-#запрашиваем фото
-@bot.message_handler(content_types = ['photo'])
-def echo_photo_bot(message):
-    chat_id = message.chat.id
-    input_image = message.photo[0].file_id
-    bot.send_photo(chat_id, input_image, message.caption)   
-    bot.send_message(chat_id, 'это твое фото,что будем искать?', reply_markup=kb )
-   
-    file_info = bot.get_file(message.photo[len(message.photo)-1].file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    
-    src="D:/project/"+file_info.file_path;
-    with open(src, 'wb') as new_file:
-        new_file.write(downloaded_file)
-    
-    bot.send_photo(chat_id, downloaded_file, message.caption )   
-    bot.reply_to(message,"Фото добавлено") 
-  
-
-
-
-    
-#обработчик кнопочки
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
-    def object_detection_on_an_image(download_photo):
-            segment_image = instance_segmentation()
-            segment_image.load_model("D:\project\mask_rcnn_coco.h5")  
-            if call.data == "все объекты":
-                bot.send_message(call.message.chat.id, "мы ищем все объекты")
-
-       
-
-                segment_image.segmentImage(
-                    image_path=download_photo,
-                    show_bboxes=True,
         
-                    output_image_name=download_photo
-                )
 
+            #запрашиваем фото
+        @self.bot.message_handler(content_types = ['photo'])
+        def echo_photo_bot(message):
+            chat_id = message.chat.id
+            input_image = message.photo[0].file_id
+            self.bot.send_photo(chat_id, input_image, message.caption)   
+            self.bot.send_message(chat_id, 'это твое фото,как ты хочешь его назвать?')
+                
+            file_info = self.bot.get_file(message.photo[len(message.photo)-1].file_id)
+            downloaded_file = self.bot.download_file(file_info.file_path)
+            @self.bot.message_handler(content_types = ['text'])
+            def rewrite_name_file(message):
+                text = message.text
+                self.bot.send_message(chat_id, f"Вы назвали фото: {text}")        
+                new_file_path = "D:/project/photos/" + message.text + ".jpg"
+                print(new_file_path)
+                self.name_photo = new_file_path
+                with open(new_file_path, 'wb') as new_file:
+                    new_file.write(downloaded_file)
+                self.bot.reply_to(message,"Фото добавлено", reply_markup=self.kb)
+            ##обработчик кнопочки           
+        @self.bot.callback_query_handler(func=lambda call: True)
+        def callback_inline(call):
+            generation()
+            segment_image = instance_segmentation()
+            
+            if call.data == "все объекты":
+                self.bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text="мы ищем все объекты", reply_markup=None)
+                generation.object_detection_on_an_image(self.name_photo)
+                self.bot.send_message(call.message.chat.id, "чтобы получить результат,напишите /photo")
 
             elif call.data == "людей":
-                bot.send_message(call.message.chat.id, "мы ищем людей")
-        
+                self.bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text="мы ищем людей", reply_markup=None)
+                target_class = segment_image.select_target_classes(person=True)
+                generation.object_detection_on_an_image(self.name_photo, target_class)                  
+                self.bot.send_message(call.message.chat.id, "чтобы получить результат,напишите /photo")
 
-                segment_image.segmentImage(
-                    image_path=download_photo,
-                    show_bboxes=True,
-        
-                    output_image_name=download_photo
-                )
             elif call.data == "машины":
-                bot.send_message(call.message.chat.id, "мы ищем машины")
+                self.bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text="мы ищем машины", reply_markup=None)
+                target_class = segment_image.select_target_classes(car=True, bus=True, truck=True)
+                generation.object_detection_on_an_image(self.name_photo, target_class)
+                self.bot.send_message(call.message.chat.id, "чтобы получить результат,напишите /photo")
 
-        
-
-                segment_image.segmentImage(
-                    image_path=download_photo,
-                    show_bboxes=True,
-        
-                    output_image_name=download_photo
-                )
             elif call.data == "животных":
-           
-                bot.send_message(call.message.chat.id, "мы ищем животных")
+                self.bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text="мы ищем животных", reply_markup=None)
+                target_class = segment_image.select_target_classes(bird=True, cat=True, dog=True, horse=True, sheep=True,
+                      cow=True, elephant=True, bear=True, zebra=True,giraffe=True,)
+                generation.object_detection_on_an_image(self.name_photo, target_class)
+                self.bot.send_message(call.message.chat.id, "чтобы получить результат,напишите /photo")
+            elif call.data == "поиск":
+                generation.search_images_by_photo(self.name_photo)
+                
+        @self.bot.message_handler(commands=['photo'])
+        def photo_output_message(message):
+            output_name = open(self.name_photo,'rb')
+            print(self.name_photo)
+            self.bot.send_photo(message.chat.id,output_name,'это твое фото,и на это я потратил 2 часа', reply_markup=self.kb2)
+   
+        self.bot.polling(none_stop=True)
 
-        
+class generation(MyBot):
+    def object_detection_on_an_image(download_photo, target_choosen):
+            segment_image = instance_segmentation()
+            segment_image.load_model("mask_rcnn_coco.h5")
+            segment_image.segmentImage(
+                image_path=download_photo,
+                segment_target_classes=target_choosen,
+                show_bboxes=True,
+                output_image_name=download_photo
+            )
+    def search_images_by_photo(photo_path):
+        GCS_DEVELOPER_KEY = 'AIzaSyBkcyaos4CM74v9JhNDhVAQvjX4rjOUPbc'
+        GCS_CX = ''
+        # Инициализация GoogleImagesSearch с вашими учетными данными API
+        gis = GoogleImagesSearch('AIzaSyBkcyaos4CM74v9JhNDhVAQvjX4rjOUPbc', '553103447540-olct55v2775driffsbun6p41sftgct6p.apps.googleusercontent.com')#553103447540-olct55v2775driffsbun6p41sftgct6p.apps.googleusercontent.com
 
-                segment_image.segmentImage(
-                    image_path=download_photo,
-                    show_bboxes=True,
-        
-                    output_image_name=download_photo
-        )
+        # Выполнение поиска по фото
+        search_params = {
+            'image_url': '',
+            'img_file': photo_path,
+            'num': 10,  # Количество результатов, которые вы хотите получить
+            'safe_search': True,
+        }
 
-    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text="отправьте фото", reply_markup=None)
-#нахождение элементов
-    def main():
-        object_detection_on_an_image('photos/file_27.jpg')
+        gis.search(search_params)
+
+        # Обработка результатов поиска
+        for image in gis.results():
+            print(image.url)
+
+if __name__ == '__main__':
+    my_bot = MyBot()
+    my_bot.run()
 
 
-    if __name__ == '__main__':
-        main()
 
-@bot.message_handler(commands=['photo'])
-def photo_output_message(message):
-    output_name = open('photos/file_24.jpg','rb')
-    bot.send_photo(message.chat.id,output_name,'это твое фото,и на это я потратил 2 часа')
 
-bot.polling(none_stop=True)
